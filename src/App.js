742e3054036f75fed1dce6ad4e3bae12885858a8
MBANS8A1 <seanmbng@aol.com>
2025-10-22 11:33:13 +0100

    There are three <p> elements for the number of the question, the title, the icon (which will be used to expand and contract the AccordionItem) and the answer text. I will add the CSS styling next.

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ function Accordion() {
 
 function AccordionItem({ number, title, text }) {
   <div className="item">
-    d<p className="number">{number}</p>
-    <p className="text">{title}</p>
+    <p className="number">{number}</p>
+    <p className="title">{title}</p>
     <p className="icon">-</p>
+    <div className="content-box">{text}</div>
   </div>;
 }
 
