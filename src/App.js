16a3ff24ffa8ea90850141106e5a41f602a8972d
MBANS8A1 <seanmbng@aol.com>
2025-10-22 10:27:48 +0100

    I created the Accordion and AccordionItem components. Each AccordionItem will have a number, title and text; so, I added these properties (props) immediately and destructured them.

--- src/App.js
+++ src/App.js
@@ -2,4 +2,9 @@ function App() {
   return <div className="App"></div>;
 }
 
+function Accordion() {
+  return <div>TODO</div>;
+}
+
+function AccordionItem({ number, title, text }) {}
 export default App;
