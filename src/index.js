37768d178096bb375202d9add2d90684fbfc0e62
MBANS8A1 <seanmbng@aol.com>
2025-10-23 23:06:51 +0100

    I added a return statement that was missing from the AccordionItem functional component.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
